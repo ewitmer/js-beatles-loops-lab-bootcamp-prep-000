bc06a39d1b9393046bd9e6366121e547a178ba30
Erin Witmer <ecwitmer@gmail.com>
2017-12-08 02:47:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ function theBeatlesPlay(musicians, instruments) {
   for (let i = 0; i < musicians.length; i++) {
     newArray.push(`{musicians[i]} plays {instruments[i]}`)
   }
+  return newArray;
 }
