b95d7283d664a77161613263d73945b948e98fa4
Erin Witmer <ecwitmer@gmail.com>
2017-12-08 02:58:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ function johnLennonFacts(facts) {
 function iLoveTheBeatles(number) {
   let newArray = [];
   do {
-    newArray.push()
-  }
+    newArray.push("I love the Beatles!");
+    number++
+  } while (number < 15)
 }
